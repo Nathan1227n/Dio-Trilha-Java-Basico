f94e98f425c8086d57efdedb6a76a2b607bd81a4
Nathan Manicoba <zaphiroxd5@gmail.com>
2025-05-28 13:37:44 -0300

    adicionando o desafio ContaBanco

---
+++ Java/src/Dio/ContaBanco/ContaTerminal.java
@@ -0,0 +1,26 @@
+package Dio.ContaBanco;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a agencia de sua conta: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo do conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(" Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é "
+        + agencia + ", conta " + numero + " e seu saldo " + saldo + " ja está disponivel para saque. ");
+
+        scanner.close();
+
+    }
+}
